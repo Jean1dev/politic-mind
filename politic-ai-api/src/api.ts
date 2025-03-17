885f4d31289b7056babea7818dcc011e37d4b310
Jeanluca <jeanlucafp@gmail.com>
2025-03-17 17:33:13 -0300

    fix: update port type and refactor server start function for better error handling

--- politic-ai-api/src/api.ts
+++ politic-ai-api/src/api.ts
@@ -2,7 +2,7 @@ import Fastify from 'fastify'
 import { similaritySearch } from './lib/upstach.ts';
 
 const fastify = Fastify({ logger: true });
-const port = process.env.PORT || 8081;
+const port: any = process.env.PORT || 8081;
 
 fastify.post('/similarity-search', async (request: any, reply) => {
     try {
@@ -15,12 +15,13 @@ fastify.post('/similarity-search', async (request: any, reply) => {
     }
 });
 
-export const start = async () => {
-    try {
-        await fastify.listen({ port });
-        fastify.log.info(`Server is running at http://localhost:${port}`);
-    } catch (err) {
-        fastify.log.error(err);
-        process.exit(1);
-    }
+export const start = () => {
+    fastify.listen({ port })
+        .then(() => {
+            fastify.log.info(`Server is running at http://localhost:${port}`);
+        })
+        .catch(err => {
+            fastify.log.error(err);
+            process.exit(1);
+        });
 };
